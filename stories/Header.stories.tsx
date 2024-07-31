9f6b234d156feb37702a81984571766d2b1dd7f0
graemepirie <gpirie@dctmedia.co.uk>
2024-07-31 01:25:54 +0100

    Added basic story file.

--- stories/Header.stories.tsx
+++ stories/Header.stories.tsx
@@ -4,11 +4,15 @@ import type { Meta, StoryObj } from '@storybook/react';
 // Components
 import Header from "@/components/header/siteHeader"
 
-
 // Story config
 const meta: Meta<typeof Header> = {
-  title: 'Components/Blocks/Header',
-  component: Header,
+    title: 'Components/Blocks/Header',
+    component: Header,
+    parameters: {
+        backgrounds: {
+            default: 'light',
+        }
+    }
 };
 
 export default meta;
@@ -16,5 +20,5 @@ type Story = StoryObj<typeof Header>;
 
 // Header: Default
 export const Default: Story = {
-  render: () => <Header title={} menu={ [] } />,
+    render: () => <Header title="Graeme Pirie" menu={ [] } />,
 };
