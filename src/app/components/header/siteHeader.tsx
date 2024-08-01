e75ab339c57d9474c8b87fac050fb36ee5dba78c
graemepirie <gpirie@dctmedia.co.uk>
2024-08-01 11:25:11 +0100

    feat: Added menu icons for smaller screens. Amended classnames for styling. Added styles to the header stylesheet.

--- src/app/components/header/siteHeader.tsx
+++ src/app/components/header/siteHeader.tsx
@@ -1,26 +1,53 @@
+'use client';
+
 // Imports
+import { useState } from "react";
 import Link from "next/link";
+
+// Components
 import NavigationMenu from "@/components/menu/menu";
+import HamburgerIcon from '@/public/assets/icons/hamburger.svg';
+import CloseIcon from '@/public/assets/icons/close.svg';
+
+// Styles
 import classes from "./siteHeader.module.scss";
+
+// Types
 import { Menu } from 'types';
 
 type Props = {
     menu: Menu[];
     title: string;
+    tagline: string;
 };
 
-const SiteHeader = ({ menu, title }: Props) => {
+const SiteHeader = ({ menu, title, tagline }: Props) => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <header role="banner" className={classes['siteHeader']}>
-            <h1>
+            <h1 className={classes['logo']}>
                 <Link
                     className={classes['title']}
                     href="/">
                     {title}
+                    <span className={classes['description']}>{tagline}</span>
                 </Link>
             </h1>
-            <NavigationMenu styleClass="header-menu" menu={menu} />
+            <nav className={ classes['header-nav'] } onClick={toggleMenu}>
+
+                { menuOpen ? <CloseIcon /> : <HamburgerIcon /> }
+
+                <NavigationMenu
+                    open={menuOpen}
+                    menu={ menu }
+                />
+            </nav>
         </header>
     );
 };
