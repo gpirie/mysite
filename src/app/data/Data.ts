163ad71090f9b2c38ffc4fa93e8f260d6b35730c
graemepirie <gpirie@dctmedia.co.uk>
2024-08-21 09:22:49 +0100

    Added title to the menu component.

--- src/app/data/Data.ts
+++ src/app/data/Data.ts
@@ -160,6 +160,7 @@ export const fetchMenuByName = async ( slug: string ) => {
                         label
                         target
                         uri
+                        title
                     }
                 }
             }
