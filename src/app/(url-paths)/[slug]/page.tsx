86afa4402969b8c7909d5ddbe55bcaab4d448c39
graemepirie <gpirie@dctmedia.co.uk>
2024-08-23 01:25:35 +0100

    Added 404 check for urls that don't exist.

--- src/app/(url-paths)/[slug]/page.tsx
+++ src/app/(url-paths)/[slug]/page.tsx
@@ -2,10 +2,20 @@
 import {fetchSinglePage} from "@/data/Data";
 import {parseHTML} from "@/utils/utils";
 
+// Components
+import NotFound from "../not-found";
+
 const Page = async ({params}: { params: {slug: string}}) => {
 
     // Get Page data
     const pageData = await fetchSinglePage(params.slug);
+
+    if (!pageData) {
+
+        return (
+            <NotFound />
+        )
+    }
 
     return (
         <>
