436948e89e6f39db04d6b3ca64d48127436cff88
graemepirie <gpirie@dctmedia.co.uk>
2024-08-27 12:47:51 +0100

    Created data files separate from stories for reusability.

---
+++ stories/data/footerMenu.ts
@@ -0,0 +1,37 @@
+// Types
+import { Menu } from 'types';
+
+// Data
+export const footerMenu: Menu = {
+    id: "dGVybTo0",
+    name: "Footer",
+    menuItems: {
+        nodes: [
+            {
+                cssClasses: ["home-link"],
+                id: "cG9zdDoyMg==",
+                label: "About",
+                target: null,
+                uri: "/about/",
+                title: null,
+            },
+            {
+                cssClasses: [],
+                id: "cG9zdDoyMQ==",
+                label: "Experience",
+                target: null,
+                uri: "/experience/",
+                title: null,
+            },
+            {
+                cssClasses: [],
+                id: "cG9zdDoxNg==",
+                label: "Privacy Policy",
+                target: "_blank",
+                uri: "/privacy-policy/",
+                title: null,
+            },
+        ],
+    }
+};
+
